1ce6245121a8b2f52e61b5f3bf7e1254f16e2626
3njomzaisufi-cpu <3njomzaisufi@gmail.com>
2025-12-10 17:47:16 +0100

    Added full burger house project

---
+++ script.js
@@ -0,0 +1,20 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.add("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+});
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => menuCloseButton.click());
+});
+
+
+
+
+        
